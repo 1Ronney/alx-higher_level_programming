710725134755dd5960490459054901ad83633d8d
Winga Isaac Ronney <110449714+1Ronney@users.noreply.github.com>
2023-01-06 05:16:13 +0300

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,9 +3,12 @@
 This is the module that supplies the class LockedClass
 """
 
+
 class LockedClass:
     """
-    class for initializing LockedClass object.
-    It prevents the user from dynamically creating new instance attributes, except if the new instance attribute is called first_name
+    Class for initializing LockedClass object.
+    It prevents the user from dynamically creating new
+    instance attributes, except if the new
+    instance attribute is called first_name.
     """
-    _slots_ = ['first_name']
+    __slots__ = ['first_name']
