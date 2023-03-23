5fd8881158ca5b1e8d67129bc31769211f298e91
Winga Isaac Ronney <110449714+1Ronney@users.noreply.github.com>
2023-03-23 09:37:14 +0300

    objectsrelations

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,40 +1,23 @@
 #!/usr/bin/python3
-
 """
-This is a script that takes in the name
-of a state as an argument and lists all cities of
-that state, using the database hbtn_0e_4_usa
+script that takes in the name of a state as an argument and lists all \
+cities of that state, using the database hbtn_0e_4_usa
 """
-
-
 import MySQLdb
-import sys
+from sys import argv
+
 
 if __name__ == "__main__":
-    arg = sys.argv
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306, user=arg[1],
-        passwd=arg[2], db=arg[3],
-        charset="utf8"
-        )
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    if "'" in arg[4]:
-        idx = arg[4].index("'")
-        arg[4] = arg[4][:idx]
-    cur.execute(
-            """SELECT cities.name
-            FROM cities
-            INNER JOIN states ON states.id = cities.state_id
-            WHERE states.name = '{}'
-            ORDER BY cities.id ASC""".format(arg[4])
-            )
-    city_rows = cur.fetchall()
-    for i in range(len(city_rows)):
-        print(city_rows[i][0], end='')
-        if (i != len(city_rows) - 1):
-            print(', ', end='')
-        else:
-            print('')
+    if len(argv[4]) <= 12:
+        cur.execute("SELECT cities.name FROM cities \
+                    INNER JOIN states ON cities.state_id = states.id \
+                    WHERE states.name = '{}'\
+                    ORDER BY cities.id ASC".format(argv[4]))
+        rows = cur.fetchall()
+        cities = ", ".join(row[0] for row in rows)
+        print(cities)
     cur.close()
     conn.close()
